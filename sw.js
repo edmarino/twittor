45e53ff8c66317f876b52a19612d115cb17782c2
Edilberto Mariño Moya <edmarino@gmail.com>
2020-07-15 13:22:12 -0500

    IOS

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v4';
+const STATIC_CACHE    = 'static-v1';
 const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
