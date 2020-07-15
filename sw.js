1347e5f800a3320bd21d1ca5f58314087e69c491
Edilberto Mariño Moya <edmarino@gmail.com>
2020-07-14 22:10:02 -0500

    x

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
-//sw.js
+	   
 
-//imports
+// imports
 importScripts('js/sw-utils.js');
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+
+
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
+
 const APP_SHELL = [
-    //'/', requrido para localhost, en github NO
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
@@ -19,6 +22,7 @@ const APP_SHELL = [
     'js/app.js',
     'js/sw-utils.js'
 ];
+
 const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
@@ -27,38 +31,86 @@ const APP_SHELL_INMUTABLE = [
     'js/libs/jquery.js'
 ];
 
-//Instalación: Carga los caches
+
+
 self.addEventListener('install', e => {
-    const cacheStatic = caches.open(STATIC_CACHE).then(cache => {
-        cache.addAll(APP_SHELL);
-    }); //const cacheStatic=caches.open(STATIC_CACHE).then(cache=>{});
-    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => {
-        cache.addAll(APP_SHELL_INMUTABLE);
-    }); //const cacheInmutable=caches.open(INMUTABLE_CACHE).then(cache=>{
+																 
+								
+																	  
+																	   
+										  
+																		 
 
-    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
-}); //self.addEventListener('install',e=>{});		
+															
+											   
 
-//Activación y borrado de caches innecesarios
+    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL ));
+
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL_INMUTABLE ));
+
+
+
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
+
+});
+
+
 self.addEventListener('activate', e => {
-    const respuesta = caches.keys().then(keys => {
-        //static
-        if (key !== STATIC_CACHE && key.includes('static')) {
-            return caches.delete(key);
-        } //if(key!==CACHE_STATIC_NAME&&key.includes('static')){
-    }); //const respuesta=caches.keys().then(keys=>{
-}); //self.addEventListener('activate',e=>{
+												  
+				
+															 
+									  
+																
+													
+										   
 
-//Estrategia del fetch
-self.addEventListener('fetch', e => {
-    const respuesta = caches.match(e.request).then(res => {
-        if (res) {
+    const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+
+        });
+
+    });
+
+    e.waitUntil( respuesta );
+
+});
+
+
+
+
+self.addEventListener( 'fetch', e => {
+
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if ( res ) {
             return res;
         } else {
-            return fetch(e.request).then(newRes => {
-                return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
-            }); //return fetch(e.request).then(newRes=>{});
-        } //if(res){}else{}
-    }); //caches.match(e.request).then(res=>{})
-    e.respondWith(respuesta);
-}); //self.addEventListener('fetch',e=>{});
+
+            return fetch( e.request ).then( newRes => {
+
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+
+            });
+
+        }
+
+    });
+
+
+
+    e.respondWith( respuesta );
+
+});
+
---
+++ sw.js.o.js
@@ -0,0 +1,116 @@
+//sw.js
+
+//imports
+importScripts('js/sw-utils.js');
+
+
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+
+const APP_SHELL = [
+    //'/', requrido para localhost, en github NO
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/animate.css',
+    'js/libs/jquery.js'
+];
+
+//Instalación: Carga los caches
+
+self.addEventListener('install', e => {
+    const cacheStatic = caches.open(STATIC_CACHE).then(cache => {
+        cache.addAll(APP_SHELL);
+    }); //const cacheStatic=caches.open(STATIC_CACHE).then(cache=>{});
+    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => {
+        cache.addAll(APP_SHELL_INMUTABLE);
+    }); //const cacheInmutable=caches.open(INMUTABLE_CACHE).then(cache=>{
+
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+}); //self.addEventListener('install',e=>{});		
+
+//Activación y borrado de caches innecesarios
+								   
+
+																		
+											 
+
+
+
+																 
+
+   
+
+
+self.addEventListener('activate', e => {
+    const respuesta = caches.keys().then(keys => {
+        //static
+        if (key !== STATIC_CACHE && key.includes('static')) {
+            return caches.delete(key);
+        } //if(key!==CACHE_STATIC_NAME&&key.includes('static')){
+    }); //const respuesta=caches.keys().then(keys=>{
+}); //self.addEventListener('activate',e=>{
+
+//Estrategia del fetch
+
+							  
+
+																	
+										  
+			 
+
+																	  
+										  
+			 
+
+		   
+
+	   
+
+							 
+
+   
+
+
+
+
+self.addEventListener('fetch', e => {
+
+
+    const respuesta = caches.match(e.request).then(res => {
+
+        if (res) {
+            return res;
+        } else {
+
+            return fetch(e.request).then(newRes => {
+
+                return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+            }); //return fetch(e.request).then(newRes=>{});
+        } //if(res){}else{}
+    }); //caches.match(e.request).then(res=>{})
+		 
+
+	   
+
+
+
+    e.respondWith(respuesta);
+}); //self.addEventListener('fetch',e=>{});
+   
+
